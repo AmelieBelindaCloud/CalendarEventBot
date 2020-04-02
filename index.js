7663a5ac52e529d3c46ce43cb3f0fbade481deaf
AmelieBelindaCloud <abc0473655@gmail.com>
2020-04-02 22:00:45 +0200

    init

---
+++ index.js
@@ -0,0 +1,17 @@
+const Discord = require('discord.js')
+const { prefix, token } = require('./config/config.json')
+
+const client = new Discord.Client();
+
+client.once('ready', () => {
+    console.log('Ready')
+})
+
+client.on('message', message => {
+    console.log(message.content);
+    if(message.content.startsWith(`${prefix}create`)) {
+        message.channel.send("reply");
+    }
+})
+
+client.login(token);
