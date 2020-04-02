785d44ad110967931ba4b3c6e36657614d629e4b
AmelieBelindaCloud <abc0473655@gmail.com>
2020-04-02 23:40:16 +0200

    restructured

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js')
-const { prefix, token } = require('./config/config.json')
+const { prefix, token } = require('../config/config.json')
 
 const client = new Discord.Client();
 
